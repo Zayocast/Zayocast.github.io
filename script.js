d47bd78ec87f2aa4e13dd69a6d9509e7c840081e
Zayocast <70472130+Zayocast@users.noreply.github.com>
2021-09-24 18:44:19 +0300

    new types

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Idiot", "Developer", "Gamer", "Designer", "Freelancer", "Nigger"],
+        strings: ["Idiot", "Developer", "Gamer", "Designer", "Freelancer", "Nigger", " MEGA Nigger", " GIGA Nigger", "ULTRA Nigger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Idiot", "Developer", "Gamer", "Designer", "Freelancer", "Nigger"],
+        strings: ["Idiot", "Developer", "Gamer", "Designer", "Freelancer", "Nigger", " MEGA Nigger", " GIGA Nigger", "ULTRA Nigger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
