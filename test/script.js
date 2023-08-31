b8375c8631765ba1efb2171a9a634dca70dc7e26
Zayocast <70472130+Zayocast@users.noreply.github.com>
2023-08-31 19:13:49 +0300

    Update script.js

--- test/script.js
+++ test/script.js
@@ -62,219 +62,223 @@ var quiz = {
 		},
 		{
 			"id": 5,
-			"question": "How do you write &quot;Hello World&quot; in an alert box?",
+			"question": "Защо цъфти косата?",
 			"options": [
 				{
-					"a": "alertBox(&quot;Hello World&quot;);",
-					"b": "msg(&quot;Hello World&quot;);",
-					"c": "alert(&quot;Hello World&quot;);",
-					"d": "msgBox(&quot;Hello World&quot;);",
+					"a": "защото се третира често ",
+					"b": "насища се с кислород ",
+					"c": "защото старее ",
+					
 				}
 			],
-			"answer": "alert(&quot;Hello World&quot;);",
+			"answer": "защото старее ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 6,
-			"question": "How do you create a function in JavaScript?",
+			"question": "Колко квадрата помещение са нужни за отварянето на фризьорски салон?",
 			"options": [
 				{
-					"a": "function myFunction()",
-					"b": "function:myFunction()",
-					"c": "function = myFunction()",
+					"a": "3",
+					"b": "6",
+					"c": "9",
+					"d": "11",
 				}
 			],
-			"answer": "function myFunction()",
+			"answer": "9",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 7,
-			"question": "How do you call a function named &quot;myFunction&quot;?",
+			"question": "Кои са институциите за проверка на фризьорските салони?",
 			"options": [
 				{
-					"a": "call function myFunction()",
-					"b": "call myFunction()",
-					"c": "myFunction()",
+					"a": "РЗИ, НАП",
+					"b": "РЗИ, НАП, РСПАБ",
+					"c": "МВР, КЗП, НАП ",
 				}
 			],
-			"answer": "myFunction()",
+			"answer": "РЗИ, НАП, РСПАБ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 8,
-			"question": "How to write an IF statement in JavaScript?",
+			"question": "Кое от изброените отваря косъма? ",
 			"options": [
 				{
-					"a": "if i = 5 then",
-					"b": "if i == 5 then",
-					"c": "if (i == 5)",
-					"d": " if i = 5",
+					"a": "амоняк",
+					"b": "кислород",
+					"c": "водород ",
+					"d": "Нито едно от изброените",
 				}
 			],
-			"answer": "if (i == 5)",
+			"answer": "амоняк",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 9,
-			"question": "Which of the following is a disadvantage of using JavaScript?",
+			"question": "Кое от изброените отговаря за цвета на косата? ",
 			"options": [
 				{
-					"a": "Client-side JavaScript does not allow the reading or writing of files.",
-					"b": "JavaScript can not be used for Networking applications because there is no such support available.",
-					"c": "JavaScript doesn't have any multithreading or multiprocess capabilities.",
-					"d": "All of the above."
+					"a": "водородни връзки ",
+					"b": "кератин ",
+					"c": "меланин ",
+				
 				}
 			],
-			"answer": "All of the above.",
+			"answer": "меланин ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 10,
-			"question": "How to write an IF statement for executing some code if &quot;i&quot; is NOT equal to 5?",
+			"question": "Какво представлява дуалното боядисване?",
 			"options": [
 				{
-					"a": "if (i <> 5)",
-					"b": "if i <> 5",
-					"c": "if (i != 5)",
-					"d": "if i =! 5 then",
+					"a": "когато боядисваме корена с амонячна, а дължините с безамонячна боя ",
+					"b": "когато боядисваме в корена с безамонячна, а дължините с амонячна боя ",
+					"c": "когато боядисваме корена с тъмна боя, а дължините със светла ",
+			
 				}
 			],
-			"answer": "if (i != 5)",
+			"answer": "когато боядисваме корена с амонячна, а дължините с безамонячна боя ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 11,
-			"question": "How does a WHILE loop start?",
+			"question": "В коя част на косъма се съдържа меланина? ",
 			"options": [
 				{
-					"a": "while i = 1 to 10",
-					"b": "while (i &lt;= 10; i++)",
-					"c": "while (i &lt;= 10)"
+					"a": "кутикула ",
+					"b": "медула",
+					"c": "кортекс ",
+					"d": "Нито едно от изброените"
 				}
 			],
-			"answer": "while (i &lt;= 10)",
+			"answer": "кортекс ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 12,
-			"question": "How does a FOR loop start?",
+			"question": "Какво Ph има оксидантът? ",
 			"options": [
 				{
-					"a": "for (i = 0; i &lt;= 5)",
-					"b": "for (i = 0; i &lt;= 5; i++)",
-					"c": "for i = 1 to 5",
-					"d": "for (i &lt;= 5; i++)"
+					"a": "алкално ",
+					"b": "киселинно ",
+					"c": "неутрално",
+				
 				}
 			],
-			"answer": "for (i = 0; i &lt;= 5; i++)",
+			"answer": "киселинно ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 13,
-			"question": "How can you add a comment in a JavaScript?",
+			"question": "Как се получават третичните цветове?",
 			"options": [
 				{
-					"a": "//This is a comment",
-					"b": "&sbquo;This is a comment",
-					"c": "&lt;!--This is a comment--&gt;"
+					"a": "при смесването на два първични цвята ",
+					"b": "при смесването на равни части на един първичен и един вторичен цвят ",
+					"c": "при смесването на два вторични цвята "
 				}
 			],
-			"answer": "//This is a comment",
+			"answer": "при смесването на равни части на един първичен и един вторичен цвят ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 14,
-			"question": "How to insert a comment that has more than one line?",
+			"question": "Какво е неутрализация? ",
 			"options": [
 				{
-					"a": "/*This comment has more than one line*/",
-					"b": "//This comment has more than one line//",
-					"c": "&lt;!--This comment has more than one line--&gt;"
+					"a": "да съберем трите първични цвята в равни части и да получим кафяв (неутрален цвят)   ",
+					"b": "да съберем един първичен и един вторичен цвят ",
+					"c": "да смесим боя и оксидант "
 				}
 			],
-			"answer": "/*This comment has more than one line*/",
+			"answer": "да съберем трите първични цвята в равни части и да получим кафяв (неутрален цвят)   ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 15,
-			"question": "What is the correct way to write a JavaScript array?",
+			"question": "Кой народ полага основите на съвременното бръснарство?",
 			"options": [
 				{
-					"a": "var colors = (1:&quot;red&quot;, 2:&quot;green&quot;, 3:&quot;blue&quot;)",
-					"b": "var colors = [&quot;red&quot;, &quot;green&quot;, &quot;blue&quot;]",
-					"c": "var colors = 1 = (&quot;red&quot;), 2 = (&quot;green&quot;), 3 = (&quot;blue&quot;)",
-					"d": "var colors = &quot;red&quot;, &quot;green&quot;, &quot;blue&quot;"
+					"a": "арабите ",
+					"b": "турците ",
+					"c": "римляните ",
+					"d": "бразилците"
 				}
 			],
-			"answer": "var colors = [&quot;red&quot;, &quot;green&quot;, &quot;blue&quot;]",
+			"answer": "арабите ",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 16,
-			"question": "How do you round the number 7.25, to the nearest integer?",
+			"question": "Кое pH е неутрално?",
 			"options": [
 				{
-					"a": "rnd(7.25)",
-					"b": "Math.round(7.25)",
-					"c": "Math.rnd(7.25)",
-					"d": "round(7.25)"
+					"a": "7pH",
+					"b": "14pH",
+					"c": "0pH",
+					
 				}
 			],
-			"answer": "Math.round(7.25)",
+			"answer": "7pH",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 17,
-			"question": "How do you find the number with the highest value of x and y?",
+			"question": "Кое pH е алкално?",
 			"options": [
 				{
-					"a": "Math.max(x, y)",
-					"b": "Math.ceil(x, y)",
-					"c": "top(x, y)",
-					"d": "ceil(x, y)"
+					"a": "По-малко от 7pH",
+					"b": "По-голямо от 7pH",
+					"c": "4pH",
+				
 				}
 			],
-			"answer": "Math.max(x, y)",
+			"answer": "По-голямо от 7pH",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 18,
-			"question": "What is the correct JavaScript syntax for opening a new window called &quot;w2&quot;?",
+			"question": "Меланина отговаря за ?",
 			"options": [
 				{
-					"a": "w2 = window.new(&quot;http://www.w3schools.com&quot;);",
-					"b": "w2 = window.open(&quot;http://www.w3schools.com&quot;);"
+					"a": "Цвета на косата,кожата,очите",
+					"b": "Здравината на косъма",
+					"c": "Гъвкавост на косъма"
 
 				}
 			],
-			"answer": "w2 = window.open(&quot;http://www.w3schools.com&quot;);",
+			"answer": "Цвета на косата,кожата,очите",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 19,
-			"question": "JavaScript is the same as Java.",
+			"question": "Химичен състав на косъма ?",
 			"options": [
 				{
-					"a": "true",
-					"b": "false"
+					"a": "Калций,Водород,Калий,Сяра,Кислород",
+					"b": "Въглерод,Кислород,Азот,Водород,Сяра",
+					"c": "Магнезий,Въглерод,Калций,Хром,Азот",
 
 				}
 			],
-			"answer": "false",
+			"answer": "Въглерод,Кислород,Азот,Водород,Сяра",
 			"score": 0,
 			"status": ""
 		},
@@ -294,75 +298,119 @@ var quiz = {
 		},
 		{
 			"id": 21,
-			"question": "Which event occurs when the user clicks on an HTML element?",
+			"question": "Колко минути се стои с къдрина?",
 			"options": [
 				{
-					"a": "onchange",
-					"b": "onclick",
-					"c": "onmouseclick",
-					"d": "onmouseover"
+					"a": "45мин.",
+					"b": "Определя се според състоянието на косата",
+					"c": "10мин.",
+					"d": "30мин."
 				}
 			],
-			"answer": "onclick",
+			"answer": "Определя се според състоянието на косата",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 22,
-			"question": "How do you declare a JavaScript variable?",
+			"question": "Колко време се стои с фиксажа?",
 			"options": [
 				{
-					"a": "var carName;",
-					"b": "variable carName;",
-					"c": "v carName;"
+					"a": "10мин.",
+					"b": "45мин.",
+					"c": "15мин."
 				}
 			],
-			"answer": "var carName;",
+			"answer": "10мин.",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 23,
-			"question": "Which operator is used to assign a value to a variable?",
+			"question": "Какво се случва с косата по време на къдрене?",
 			"options": [
 				{
-					"a": "*",
-					"b": "-",
-					"c": "=",
-					"d": "x"
+					"a": "Променя се структурата на косъма",
+					"b": "Подхранва се",
+					"c": "Насища се с кислород",
+					
 				}
 			],
-			"answer": "=",
+			"answer": "Променя се структурата на косъма",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 24,
-			"question": "What will the following code return: Boolean(10 &gt; 9)",
+			"question": "Какво представлява коректорът?",
 			"options": [
 				{
-					"a": "NaN",
-					"b": "false",
-					"c": "true"
+					"a": "Неотрализира или подсилва цвета",
+					"b": "Подхранва косата при боядисване",
+					"c": "Ламинира косъма"
 				}
 			],
-			"answer": "true",
+			"answer": "Неотрализира или подсилва цвета",
 			"score": 0,
 			"status": ""
 		},
 		{
 			"id": 25,
-			"question": "Is JavaScript case-sensitive?",
+			"question": "Колко минути се стои с амонячна боя?",
 			"options": [
 				{
-					"a": "No",
-					"b": "Yes"
+					"a": "45мин.",
+					"b": "30мин.",
+					"c": "10мин."
 				}
 			],
-			"answer": "Yes",
+			"answer": "30мин.",
+			"score": 0,
+			"status": ""
+		},
+		{
+			"id": 26,
+			"question": "Колко време се стои с безамонячна боя? ",
+			"options": [
+				{
+					"a": "35мин.",
+					"b": "60мин.",
+					"c": "45мин."
+				}
+			],
+			"answer": "45мин.",
+			"score": 0,
+			"status": ""
+		},
+		{
+			"id": 27,
+			"question": "Колко време се стои с мъжка боя за коса?",
+			"options": [
+				{
+					"a": "10мин.",
+					"b": "16мин.",
+					"c": "30мин."
+				}
+			],
+			"answer": "10мин.",
+			"score": 0,
+			"status": ""
+		},
+		{
+			"id": 28,
+			"question": "Какво означава ламиниране на косата?",
+			"options": [
+				{
+					"a": "Обиране на цъфтежа.",
+					"b": "Възтановяване структурата на косъма,чрез терапия.",
+				
+				}
+			],
+			"answer": "Възтановяване структурата на косъма,чрез терапия.",
 			"score": 0,
 			"status": ""
 		}
+		
 	]
 }
 
