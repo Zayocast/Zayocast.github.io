b69958aa3456e129aacd5cba5b7ecbe551e3b3ee
Zayocast <70472130+Zayocast@users.noreply.github.com>
2025-02-02 21:50:41 +0200

    fixes

--- vodazamen/app.js
+++ vodazamen/app.js
@@ -126,7 +126,7 @@ function formatDate(date) {
   const day = String(d.getDate()).padStart(2, '0');
   const month = String(d.getMonth() + 1).padStart(2, '0');
   const year = d.getFullYear();
-  return `${day}-${month}-${year}`;
+  return `${day}.${month}.${year}`;
 }
 
 // Функция за промяна на статус на поръчка
