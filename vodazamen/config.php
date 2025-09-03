010fc415183a3f2231808b6397e6750c7da5dd63
Zayocast <70472130+Zayocast@users.noreply.github.com>
2025-09-03 15:44:54 +0300

    moved
    wrong repository

--- vodazamen/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('APP_VERSION', 'v2.1.1a');
-?>
--- vodazamen/head.php
+++
@@ -1,4 +0,0 @@
-<div class="bg-gray-100 p-2 flex items-center justify-center space-x-2 rounded-t-lg shadow">
-    <img src="newlogo2.png" alt="Logo" class="h-16">
-    <h1 class="text-2xl font-bold text-gray-700">Vodazamen Manager</h1>
-</div>
